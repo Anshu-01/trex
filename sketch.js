c83c4d486a1f949e41cf577d7d4d930cdd70922b
Anshu-01 <73426618+Anshu-01@users.noreply.github.com>
2020-11-07 13:23:28 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,13 @@
+function setup() {
+  
+  createCanvas(400, 400);
+  
+}
+
+function draw() {
+  background('black');
+  rect(10,160,10,70);
+  rect(380,mouseY,10,70);
+  rect(195,195,10,10);
+  
+}
